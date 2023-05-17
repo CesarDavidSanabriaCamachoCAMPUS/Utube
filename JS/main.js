a6d920d89df52764c3602365678b7ef48cf337db
CesarDavidSanabriaCamachoCAMPUS <cesar.sanabria1307@gmail.com>
2023-05-17 17:39:04 -0500

    feat: :tada: utube
    api de you tube

---
+++ JS/main.js
@@ -0,0 +1,78 @@
+/* 
+   programa para consumir una API de youtube que permita ver el video, el titulo
+   los videos relacionados, la foto y el nombre del canal del video y por ultimo
+   los comentarios y la descripcion de el mismo 
+*/
+// Autor: Cesar David Sanabria Camacho
+// fecha: 17/05/2023
+
+// declaracion de variables
+let iframe = document.querySelector('iframe');
+let videoTitle = document.querySelector('.title')
+let photoChannel = document.querySelector('.photoChannel')
+let nameChannelText = document.querySelector('.nameChannelText')
+let Container = document.querySelector('.feedback')
+let videoDescription = document.querySelector('.videoDescription')
+
+// variable que recibe la informacion del search por medio de la accion "click" en el "button"
+let button = document.querySelector('.button');
+button.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    const data = document.querySelector('.search').value;
+    console.log(data)
+    getVideos(data)
+})
+
+// variable constante que contiene el metodo GET para traer la informacion de la API 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8dea6af0demsha5896400295d7c0p1f2eadjsn372ee8ed211a',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
+
+// variable constante para llamar la informacion de los videos de la API
+const getVideos = async (data) => {
+    const videos = await (
+      await fetch(
+        `https://youtube138.p.rapidapi.com/search/?q=${data}&hl=en&gl=US`,
+        options
+      )
+    ).json();
+   
+    iframe.src = `https://www.youtube.com/embed/${videos.contents[0].video.videoId}`
+
+    videoTitle.innerHTML = videos.contents[0].video.title;
+
+    photoChannel.src = videos.contents[0].video.author.avatar[0].url;
+
+    nameChannelText.innerHTML = videos.contents[0].video.author.title;
+
+    videoDescription.innerHTML = videos.contents[0].video.descriptionSnippet
+
+    // variable constante para llamar la informacion de los comentarios de la API
+    const comentarios = await (
+      await fetch(
+        `https://youtube138.p.rapidapi.com/video/comments/?id=${videos.contents[0].video.videoId}&hl=en&gl=US`,
+        options
+      )
+    ).json();
+
+    // ciclo for para consumir los comentarios necesarios de la API
+    for (let x = 0; x < comentarios.comments.length; x++) {
+      const div = document.createElement('div');
+      div.setAttribute('class', 'feedbackItem');
+
+      div.innerHTML = `
+        <h5 class="feedbackName">${comentarios.comments[x].author.title}</h5>
+        <p class="feedbackContent">${comentarios.comments[x].content}</p>
+    `;
+
+    Container.appendChild(div)
+
+    }
+
+  };
+
